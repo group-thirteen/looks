a5b5fb775f3dbe184adde82ef796ce4e3baf9ee3
Vincent Huang <vincent.kim.huang@gmail.com>
2020-03-11 09:23:44 -0700

    Reformat directory to remove 'dist' and add 'public'
    - Move 'index.html' to 'public'
    - Start defining index.jsx

---
+++ client/src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Comonent {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(<App />, document.getElementbyId('app'));
