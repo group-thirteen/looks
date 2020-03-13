5f7cbff2762bba7c3966ab7236ab0f849c8c51f3
Vincent Huang <vincent.kim.huang@gmail.com>
2020-03-13 11:12:11 -0700

    Add equality test to carousel for CircleCI

---
+++ reactTests/tests/carousel.spec.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../../client/src/components/App.jsx';
+
+describe('Testing environment set-up', () => {
+  it('"npm test" should run jest correctly', () => {
+    expect(true).toEqual(true);
+  });
+});
