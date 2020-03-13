3101214886b86ea551256bc81e7b5e5783deecc6
Vincent Huang <vincent.kim.huang@gmail.com>
2020-03-13 11:18:24 -0700

    Add one passing test to carousel.spec.js for CircleCI

--- reactTests/tests/carousel.spec.js
+++ reactTests/tests/carousel.spec.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../../client/src/components/App.jsx';
-
 describe('Testing environment set-up', () => {
   it('"npm test" should run jest correctly', () => {
     expect(true).toEqual(true);
