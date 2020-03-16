e1fb7d34afb65fb66eeb76dd15e39b936cbc7703
Vincent Huang <vincent.kim.huang@gmail.com>
2020-03-15 21:49:58 -0700

    Write App & Carousel tests

---
+++ database/exampleDbEntry.js
@@ -0,0 +1,104 @@
+const exampleDbEntry = {
+	"_id" : ObjectId("5e6f0504f0262ede565750ce"),
+	"bottoms" : [
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/bottoms/da540d04-120e-453b-8b4b-3e48cb65e70f.jpeg",
+			"price" : "$406.00"
+		},
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/bottoms/da540d04-120e-453b-8b4b-3e48cb65e70f.jpeg",
+			"price" : "$406.00"
+		},
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/bottoms/8aa637ef-326c-42f6-a68d-eebfc409f14a.jpeg",
+			"price" : "$230.00"
+		}
+	],
+	"belts" : [
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/belts/b30838fe-51e0-4ecd-a567-2aa2c18d2177.jpeg",
+			"price" : "$211.00"
+		},
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/belts/1613c17f-fe3c-41c2-b030-b679cec21129.jpeg",
+			"price" : "$446.00"
+		},
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/belts/8e44a692-c2d0-4742-a4c3-d155a12f5aa6.jpeg",
+			"price" : "$765.00"
+		}
+	],
+	"bags" : [
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/bags/9f51c9b0-6c96-4ae9-8371-c6c6dc230fd4.jpeg",
+			"price" : "$136.00"
+		},
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/bags/03428a32-efaf-466b-9781-9a36f74e5b54.jpeg",
+			"price" : "$265.00"
+		},
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/bags/b2e2a874-31a5-4720-92b5-37d27f13b924.jpeg",
+			"price" : "$994.00"
+		}
+	],
+	"jewelry" : [
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/jewelry/50da521c-fcc1-42a0-b3a5-772bffa94235.jpeg",
+			"price" : "$60.00"
+		},
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/jewelry/c0482369-0721-43d7-ba22-09d51727714d.jpeg",
+			"price" : "$833.00"
+		},
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/jewelry/5d9ed29a-97a1-4f12-be12-c206fd1013c0.jpeg",
+			"price" : "$293.00"
+		}
+	],
+	"outerwear" : [
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/outerwear/6954c084-41d5-49f9-9cce-a73242e2a5a1.jpeg",
+			"price" : "$763.00"
+		},
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/outerwear/4a1a54af-b097-43e1-b26c-9485aa4c84a8.jpeg",
+			"price" : "$625.00"
+		},
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/outerwear/88784edc-0237-4331-98ab-43803be43440.jpeg",
+			"price" : "$939.00"
+		}
+	],
+	"shoes" : [
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/shoes/cac2c34d-bd8e-4b00-b379-a731bcb2b45c.jpeg",
+			"price" : "$134.00"
+		},
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/shoes/cac2c34d-bd8e-4b00-b379-a731bcb2b45c.jpeg",
+			"price" : "$134.00"
+		},
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/shoes/87a7d9ef-ed79-4d2b-852b-8540c555142f.jpeg",
+			"price" : "$73.00"
+		}
+	],
+	"tops" : [
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/tops/9a2dbd93-6fa4-4cc5-bd8b-940bfb6c4f76.jpeg",
+			"price" : "$203.00"
+		},
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/tops/b03bace5-3a8b-4f8e-b622-46589d9fcbea.jpeg",
+			"price" : "$372.00"
+		},
+		{
+			"url" : "https://hrsf126-looks-fec.s3-us-west-1.amazonaws.com/fec-imagery/tops/95e7393c-7ed1-44e4-8ffe-1c032b4cb86b.jpeg",
+			"price" : "$789.00"
+		}
+	],
+	"__v" : 0
+}
+
+export default exampleDbEntry;
