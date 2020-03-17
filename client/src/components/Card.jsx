1ded92851008210c0fd3487ff1fa4c50a16c67ff
Vincent Huang <vincent.kim.huang@gmail.com>
2020-03-16 20:32:41 -0700

    Resize service and images

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const Card = (props) => (
   <div className="card" test="card">
-    <img src={props.product.url} />
+    <img className="productimg" src={props.product.url} />
     <p>{props.product.price}</p>
   </div>
 );
