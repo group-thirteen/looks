7be92231bc1de9109ec6fd7464606f332e013636
Vincent Huang <vincent.kim.huang@gmail.com>
2020-03-18 14:25:12 -0700

    Resize service

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,9 @@ import styles from '../styles/Header.css';
 const Header = ({ title, user }) => (
   <div className={styles.header} test="header">
     <h1 className={styles.lookname}>{title}</h1>
-    <p className={styles.username}>{user}</p>
+    <h2 className={styles.username}>
+      <span className={styles.underline}>{user}</span>
+    </h2>
   </div>
 );
 
